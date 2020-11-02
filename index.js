271836b23d15a4b40ec82f47b23d008f6b1caaba
manav2401 <manavdarji.india@gmail.com>
2020-11-02 18:12:10 +0530

    refactor: modified directory structure

--- index.js
+++ index.js
@@ -1,30 +1,47 @@
+// grabbing the packages
 const express = require('express');
 const WebSocket = require('ws');
 const http = require('http');
+const path = require('path');
+
+// app variables
 var app = null;
 var httpServer = null;
 var webSockerServer = null;
-var connections = [];
 var clientId = 0;
 var msg = null;
-const port = 5000;
 
+// maintaining the connections array to track client connections
+var connections = [];
+
+const PORT = 5000;
+
+// function to log by timestamp
 function log(text) {
   var time = new Date();
   console.log("[" + time.toLocaleTimeString() + "] [Server]" + text);
 }
 
+// create an express app object
 app = express();
+
+// creating http server and passing the express app object
 httpServer = http.createServer(app);
 
+// allow the app to load static content
+app.use(express.static(path.join(__dirname, "./public")));
+
+// defining the '/' route for loading the base index.html file
 app.get('/', (req, res) => {
-  res.sendFile('index.html', { root: __dirname + '/public' });
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 })
 
-httpServer.listen(port, () => {
-  console.log('Server listening on http://localhost:' + port);
+// starting the server, listening on `PORT` port.
+httpServer.listen(PORT, () => {
+  console.log('Server listening on http://localhost:' + PORT);
 })
 
+// creating a new websocket server
 webSockerServer = new WebSocket.Server({
   server: httpServer
 });
@@ -33,24 +50,28 @@ if (!webSockerServer) {
   log('ERROR: Unable to create websocet server');
 }
 
+// on connection listener
 webSockerServer.on('connection', function (ws) {
 
+  // connection has websocket object and ID assigned of the client
   var connection = {};
   connection.webSocket = ws;
   connection.clientId = clientId;
   log('Client connected. ID: ' + clientId);
   clientId++;
 
-  // store into connections array
+  // add the object to the array
   connections.push(connection);
 
-  // send the client the id assigned
+  // send the client with it's assigned client id
   msg = {
     type: "id",
     id: connection.clientId
   };
   ws.send(JSON.stringify(msg));
 
+
+  // on message listner
   ws.on('message', function (message) {
 
     // parse the message to json
@@ -61,11 +82,13 @@ webSockerServer.on('connection', function (ws) {
       log(messageReceived.message + " from " + messageReceived.id);
     }
 
+    // message when caller offers a call to the callee
     if (messageReceived.type === 'video-offer-request') {
 
       var i;
       var callee = null;
 
+      // find the corresponding callee from the array
       for (i = 0; i < connections.length; i++) {
         if (messageReceived.calleeId === connections[i].clientId) {
           callee = connections[i].webSocket;
@@ -75,6 +98,7 @@ webSockerServer.on('connection', function (ws) {
 
       if (callee) {
 
+        // send the message to corresponding callee
         msg = {
           type: "video-answer-request",
           callerId: messageReceived.id,
@@ -83,6 +107,7 @@ webSockerServer.on('connection', function (ws) {
         callee.send(JSON.stringify(msg));
 
       } else {
+
         // send error message
         msg = {
           type: "error",
@@ -94,11 +119,13 @@ webSockerServer.on('connection', function (ws) {
 
     }
 
+    // receives the response of the call offer from callee
     if (messageReceived.type === 'client-reply') {
 
       var i;
       var caller = null;
 
+      // find the corresponding caller from the array
       for (i = 0; i < connections.length; i++) {
         if (messageReceived.callerId === connections[i].clientId) {
           caller = connections[i].webSocket;
@@ -107,6 +134,8 @@ webSockerServer.on('connection', function (ws) {
       }
 
       if (caller) {
+
+        // sends the response to the corresponding caller
         msg = {
           type: "server-reply",
           reply: messageReceived.reply,
@@ -114,7 +143,10 @@ webSockerServer.on('connection', function (ws) {
           answer: messageReceived.answer
         }
         caller.send(JSON.stringify(msg));
+
       } else {
+
+        // sends the error message
         msg = {
           type: "error",
           message: "Caller ID " + messageReceived.callerId + " doesn't exist."
@@ -122,14 +154,15 @@ webSockerServer.on('connection', function (ws) {
         ws.send(JSON.stringify(msg));
       }
 
-
     }
 
+    // receives the call close request from any client
     if (messageReceived.type === 'close-call-client') {
 
       var i;
       var client = null;
 
+      // finds the other client
       for (i = 0; i < connections.length; i++) {
         if (messageReceived.clientId === connections[i].clientId) {
           client = connections[i].webSocket;
@@ -139,6 +172,7 @@ webSockerServer.on('connection', function (ws) {
 
       if (client) {
 
+        // forwards the message to the corresponding other client
         msg = {
           type: 'close-call-server',
           clientId: messageReceived.id
@@ -147,6 +181,7 @@ webSockerServer.on('connection', function (ws) {
         client.send(JSON.stringify(msg));
 
       } else {
+
         // send error message
         msg = {
           type: "error",
@@ -159,9 +194,10 @@ webSockerServer.on('connection', function (ws) {
 
   })
 
+  // on close event listnet
   ws.on('close', () => {
 
-    // removing the connection from array
+    // removing the corresponding client connection from array
     var i;
     for (i = 0; i < connections.length; i++) {
       if (ws === connections[i].webSocket) {
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,82 +1,307 @@
-const ip = "127.0.0.1";
+// defining the host and port of server
+const hostname = "localhost";
 const port = "5000";
-var myId = null;
 
-function log(text) {
-    var time = new Date();
-    console.log("[" + time.toLocaleTimeString() + "] [Client]" + text);
-}
+// client side variables
+var myId = null;
+var getCalled = true;
+var isAlreadyCalling = false;
+var clientId = null;
+var client = null;
+
+// creating an instance of RTC Peer Connection
+const { RTCPeerConnection, RTCSessionDescription } = window;
+var peerConnection = new RTCPeerConnection();
 
 // create the websocket connection
-const socket = new WebSocket('ws://' + ip + ':' + port);
+const socket = new WebSocket("ws://" + hostname + ":" + port);
+
+// get the elements for local and remote video elements
+const localVideoeElement = document.getElementById("local-video");
+const remoteVideoElement = document.getElementById("remote-video");
+
+// hide the video initially
+localVideoeElement.style.visibility = "hidden";
+remoteVideoElement.style.visibility = "hidden";
+
+// function to log by timestamp
+function log(text) {
+  var time = new Date();
+  console.log("[" + time.toLocaleTimeString() + "] [Client]" + text);
+}
+
+// function to display the client ID assigned by server
+async function showClientId() {
+
+  // check if ID is valid
+  if (myId != null) {
+    const idElement = document.getElementById("client-id");
+    const newElement = document.createElement("p");
+    newElement.innerHTML = `<h3>Your User ID is ${myId}</h3>`;
+    idElement.appendChild(newElement);
+  } else {
+    console.warn("Unable to fetch client id.");
+  }
+}
+
+// send message on click event for testing
+function sendMessage() {
+  var msg = {
+    type: "normal",
+    id: myId,
+    message: "Hello Server!",
+  };
+  socket.send(JSON.stringify(msg));
+};
+
+// call user on click event
+function requestCall() {
+
+  log("Value entered: " + document.getElementById("callee-id").value);
+  var calleeId = document.getElementById("callee-id").value;
+
+  // check if input is valid or not
+  if (calleeId === "") {
+    // empty string
+    log("entered empty string!");
+  } else {
+    makeCall(calleeId);
+  }
+
+};
+
+// function for initiating the call
+async function makeCall(clientId) {
+
+  // create offer and sending to corresponding client
+  clientId = Number(clientId);
+  const offer = await peerConnection.createOffer();
+  await peerConnection.setLocalDescription(
+    new RTCSessionDescription(offer)
+  );
+
+  // sending request to server for client ID entered
+  var msg = {
+    type: "video-offer-request",
+    id: myId,
+    calleeId: clientId,
+    offer: offer,
+  };
+  socket.send(JSON.stringify(msg));
+
+}
+
+// end call event listner
+async function endCallEvent() {
+  // flag = 0
+  endCall(0);
+};
+
+// function for closing the call and updating status and informing peer
+async function endCall(flag) {
+
+  // stop the remote video, set stream source to null
+  const remoteVideo = document.getElementById("remote-video");
+  if (remoteVideo) {
+    remoteVideo.srcObject = null;
+  }
+
+  // update variables
+  getCalled = true;
+  isAlreadyCalling = false;
+
+  // hide video elements
+  localVideoeElement.style.visibility = "hidden";
+  remoteVideoElement.style.visibility = "hidden";
+
+  // set on call message
+  const onCallMessage = document.getElementById("on-call-message");
+  onCallMessage.innerHTML = `<h3>Disconnected with User ID: ${clientId}</h3>`;
+
+  // clear peer connection
+  peerConnection.close();
+  peerConnection = null;
+
+  // create new peer connection for future calls
+  peerConnection = new RTCPeerConnection();
+
+  // set the local and remote stream listnets
+  setStreams();
+
+  if (flag == 0) {
+    // send message to other client for closing call
+    msg = {
+      type: "close-call-client",
+      id: myId,
+      clientId: clientId,
+    };
+    socket.send(JSON.stringify(msg));
+  }
+
+  // clear the client ID stored
+  clientId = null;
+}
+
+// sets the remote and local stream on client side
+function setStreams() {
+
+  // on track listner, runs when track is obtained from peer
+  // stores the stream in source object of HTML element  
+  peerConnection.ontrack = function ({ streams: [stream] }) {
+    const remoteVideo = document.getElementById("remote-video");
+    if (remoteVideo) {
+      remoteVideo.srcObject = stream;
+    }
+  };
+
+  // get audio and video for local stream
+  navigator.getUserMedia(
+    { video: true, audio: true },
+    (stream) => {
+
+      // set the local stream
+      const localVideo = document.getElementById("local-video");
+      if (localVideo) {
+        localVideo.srcObject = stream;
+      }
+      console.log("User Media Set.");
+
+      // add the tracks to the peer connection for stream transfer
+      stream
+        .getTracks()
+        .forEach((track) => peerConnection.addTrack(track, stream));
+    },
+    (error) => {
+      console.warn(error.message);
+    }
+  );
+}
 
 // incoming message event listner
-socket.addEventListener('message', (event) => {
+socket.addEventListener("message", async (event) => {
 
-    log(event.data);
+  // log(event.data);
 
-    // parse the message to json
-    var messageReceived = JSON.parse(event.data);
+  // parse the message to json
+  var messageReceived = JSON.parse(event.data);
 
-    // store client id
-    if (messageReceived.type === 'id') {
-        myId = messageReceived.id;
-    }
+  // store client id
+  if (messageReceived.type === "id") {
+    myId = messageReceived.id;
+    await showClientId();
+  }
 
-    if (messageReceived.type === 'video-answer-request') {
-        log('Offer received form client: ' + messageReceived.callerId);
-        
-        // prompt user for accept/reject
-        var result = confirm('Accept call from client ID: ' + messageReceived.callerId + '?');
-        result = result ? "1" : "0";
+  // request from caller
+  if (messageReceived.type === "video-answer-request") {
 
+    log("Offer received form client: " + messageReceived.callerId);
+
+    var result = "1";
+    var answer = null;
+
+    if (getCalled) {
+
+      // prompt user for accept/reject
+      result = window.confirm(
+        "Accept call from client ID: " + messageReceived.callerId + "?"
+      );
+      result = result ? "1" : "0";
+
+      if (result === "0") {
         var msg = {
-            type: "client-reply",
-            reply: result,
-            id: myId,
-            callerId: messageReceived.callerId
-        }
+          type: "client-reply",
+          reply: result,
+          id: myId,
+          callerId: messageReceived.callerId,
+          answer: null,
+        };
         socket.send(JSON.stringify(msg));
+      }
     }
 
-    if (messageReceived.type === 'error') {
-        log(messageReceived.message);
-    }
+    if (result === "1") {
+      await peerConnection.setRemoteDescription(
+        new RTCSessionDescription(messageReceived.offer)
+      );
+      answer = await peerConnection.createAnswer();
+      await peerConnection.setLocalDescription(
+        new RTCSessionDescription(answer)
+      );
 
-    if (messageReceived.type === 'server-reply') {
-        // reply from server about acceptance/rejection
-        if (messageReceived.reply === '1') {
-            log('Callee with ID: ' + messageReceived.calleeId + ' accepted your call.');
-        } else {
-            log('Callee with ID: ' + messageReceived.calleeId + ' rejected your call.');
-        }
-    }
+      // set variables
+      getCalled = false;
+      clientId = messageReceived.callerId;
 
-})
+      // show video elements
+      localVideoeElement.style.visibility = "visible";
+      remoteVideoElement.style.visibility = "visible";
 
-const sendMessage = () => {
-    var msg = {
-        type: "normal",
+      // set on call message
+      const onCallMessage = document.getElementById("on-call-message");
+      onCallMessage.innerHTML = `<h3>On call with User ID: ${clientId}</h3>`;
+
+      // send the response back
+      var msg = {
+        type: "client-reply",
+        reply: result,
         id: myId,
-        message: "Hello Server!"
+        callerId: messageReceived.callerId,
+        answer: answer,
+      };
+      socket.send(JSON.stringify(msg));
     }
-    socket.send(JSON.stringify(msg));
-}
+  }
 
-const requestCall = () => {
-    log('Value entered: ' + document.getElementById('callee-id').value);
-    var calleeId = document.getElementById('callee-id').value;
-    if (calleeId === '') {
-        // empty string
-        log('entered empty string!')
+  // error message
+  if (messageReceived.type === "error") {
+    log(messageReceived.message);
+  }
+
+  // response from server, regarding acceptance/rejection of call
+  if (messageReceived.type === "server-reply") {
+
+    if (messageReceived.reply === "1") {
+      log(
+        "Callee with ID: " +
+        messageReceived.calleeId +
+        " accepted your call."
+      );
+
+      await peerConnection.setRemoteDescription(
+        new RTCSessionDescription(messageReceived.answer)
+      );
+
+      clientId = messageReceived.calleeId;
+
+      // show video elements
+      localVideoeElement.style.visibility = "visible";
+      remoteVideoElement.style.visibility = "visible";
+
+      // set on call message
+      const onCallMessage = document.getElementById("on-call-message");
+      onCallMessage.innerHTML = `<h3>On call with User ID: ${clientId}</h3>`;
+
+      if (!isAlreadyCalling) {
+        makeCall(messageReceived.calleeId);
+        isAlreadyCalling = true;
+      }
+
     } else {
-        calleeId = Number(calleeId);
-        var msg = {
-            type: "video-offer-request",
-            id: myId,
-            calleeId: calleeId
-        }
-        socket.send(JSON.stringify(msg));
+      log(
+        "Callee with ID: " +
+        messageReceived.calleeId +
+        " rejected your call."
+      );
     }
+  }
 
-}
+  // request for closing the call
+  if (messageReceived.type === "close-call-server") {
+    // flag = 1
+    endCall(1);
+  }
+
+});
+
+// setting the streams initially
+setStreams();
